4f0b56cf2e8d66ac906d7fc3fbef67d2b40788c3
legendgamingusa <86386751+legendgamingusa@users.noreply.github.com>
2022-09-05 03:01:26 -0500

    V1

---
+++ README.md
@@ -0,0 +1,5 @@
+Discord bot that announces when specific stream from trovo.live is live with user input.
+
+Index and deploy-commands files are taken from discord js guide, its very standard so you can create commands, etc.
+
+**Bot is not hosted somewhere at the moment.**
---
+++ commands/islive.js
@@ -0,0 +1,51 @@
+const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const axios = require('axios');
+
+const dotenv  = require('dotenv');
+dotenv.config();
+
+
+module.exports = {
+    data:new SlashCommandBuilder()  //create slash command
+    .setName('live')
+    .setDescription('Trigger bot message on live')
+    .addStringOption(option =>        //create user input for the slash command
+        option.setName('input')
+        .setDescription('user input')
+        .setRequired(true)
+    ),
+async execute(interaction){
+    const inputt = interaction.options.getString('input');
+    const input = inputt.toUpperCase();   // get user input
+    const config = {                                        //get the config with the options for the api request
+        method: 'POST',
+        url: 'https://open-api.trovo.live/openplatform/channels/id',
+        headers: {
+        'Accept': 'application/json',
+        'Client-ID': process.env.CLIENT_ID,                     //you need to create a .env file with your client-id so its safe, you also need dotenv install
+        'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data:`{\"username\":\"${input}\"}`
+    };
+  await axios(config)
+.then(response=> {
+    const data = response.data;
+
+    if (data.is_live==true){                  //if stream is live create the embed 
+        const embed = new MessageEmbed()
+        .setAuthor({name: `${input} is now live on Trovo!`, url:`https://trovo.live/${input}`})
+        .setTitle((data.live_title).toString())
+        .setURL(`https://trovo.live/${input}`)
+        .addField('Category',data.category_name,true)
+        .addField('Viewers',(data.current_viewers).toString(),true)
+        .setImage(data.thumbnail)
+        .setFooter({text:'Author: Kuedon#4714'})
+        interaction.reply({embeds: [embed]});
+        }else{
+            interaction.reply("Stream is not live(yet), check back later.");
+        }
+})
+.catch(err=>console.log(err));
+    },
+};
